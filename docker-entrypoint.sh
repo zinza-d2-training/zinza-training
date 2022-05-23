ed6f86f437b5fce24f6d18f3d13d48ff65fa322e
hungnp194 <hungnp194@gmail.com>
2022-05-23 22:13:53 +0700

    Fix path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,6 @@ if [ ${APP_ENV} = "production" ]; then
 else
     # Bundle install
     yarn install
-    yarn add @next/swc-win32-x64-msvc @next/swc-linux-arm64-gnu @next/swc-linux-arm64-musl --ignore-platform
     yarn run dev
 fi
 
