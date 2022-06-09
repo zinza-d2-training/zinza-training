c9bb2144523afb54f3b05a61a14510962b36dc3c
hungnp194 <hungnp194@gmail.com>
2022-06-09 21:39:35 +0700

    Add description meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="description" content="Tools for training purpose" key="description" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
