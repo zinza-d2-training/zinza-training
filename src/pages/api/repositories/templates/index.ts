9c5ccbdae6c957ac234647539bde95afda54fa84
hungnp194 <hungnp194@gmail.com>
2022-07-28 11:04:33 +0700

    Temporary disable cache

--- src/pages/api/repositories/templates/index.ts
+++ src/pages/api/repositories/templates/index.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { createGithubAdminClient } from 'src/libs/octokit';
 import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types';
 import { createApiErrorResponse, createApiSuccessResponse } from 'src/pages/api/index';
-import cache from 'memory-cache';
 
 export type RepositoriesTemplatesResponse =
   RestEndpointMethodTypes['search']['repos']['response']['data']['items'];
@@ -11,25 +10,16 @@ export type RepositoriesTemplatesResponse =
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const githubAdminClient = createGithubAdminClient();
   try {
-    const cachedTemplates = cache.get('templates');
-    if (cachedTemplates) {
-      createApiSuccessResponse<RepositoriesTemplatesResponse>({
-        data: cachedTemplates,
-        res
-      });
-    } else {
-      const reposResponse = await githubAdminClient?.rest.search.repos({
-        q: `template-- in:name org:${process.env.NEXT_PUBLIC_ORG}`
-      });
-      const templates = reposResponse.data.items.filter(
-        (template) => template.name !== 'training-template'
-      );
-      cache.put('templates', templates, 5 * 60 * 1000);
-      createApiSuccessResponse<RepositoriesTemplatesResponse>({
-        data: templates,
-        res
-      });
-    }
+    const reposResponse = await githubAdminClient?.rest.search.repos({
+      q: `template-- in:name org:${process.env.NEXT_PUBLIC_ORG}`
+    });
+    const templates = reposResponse.data.items.filter(
+      (template) => template.name !== 'training-template'
+    );
+    createApiSuccessResponse<RepositoriesTemplatesResponse>({
+      data: templates,
+      res
+    });
   } catch (e) {
     createApiErrorResponse<null>({ res });
   }
